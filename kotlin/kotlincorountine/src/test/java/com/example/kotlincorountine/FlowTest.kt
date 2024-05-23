4ca9a327e864ef352bc57606c8b8eb88e2c6ac46
Huff <1107018203@qq.com>
2024-05-24 07:45:54 +0800

    flow学习

--- kotlin/kotlincorountine/src/test/java/com/example/kotlincorountine/FlowTest.kt
+++ kotlin/kotlincorountine/src/test/java/com/example/kotlincorountine/FlowTest.kt
@@ -1,11 +1,17 @@
-package com.example.kotlincoroutine
+package com.example.kotlincorountine
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeoutOrNull
 import org.junit.Test
+import kotlin.system.measureTimeMillis
+import kotlin.time.measureTime
 
 // Flow-异步流
 // 认识：  特性  构建器与上下文  启动  取消与取消检测  缓冲
@@ -35,145 +41,209 @@ import org.junit.Test
 // 展平操作符：flatMapConcat flatMapMerge flatMapLatest
 class FlowTest {
 
-    private fun simpleFow() = flow<Int>{
-        for (i in 1..3){
-            delay(1000)
-            emit(i)
+    // 返回了多个值，但不是异步
+    fun simpleList(): List<Int> = listOf(1, 2, 3)
+
+    // 返回了多个值，是同步
+    fun simpleSequence() = sequence {
+        for (i in 1..3) {
+            Thread.sleep(1000) // 阻塞，假装在计算
+            yield(i)
         }
     }
 
-    private fun simpleFow2() = flow<Int>{
-        println("Flow started")
-        for (i in 1..3){
+    // 返回了多个值，是异步，一次性返回了多个值
+    suspend fun simpleList2(): List<Int> {
+        delay(1000)
+        return listOf(1, 2, 3)
+    }
+
+    // 返回了多个值，是异步
+    suspend fun simpleFow() = flow<Int> {
+        for (i in 1..3) {
             delay(1000)
             emit(i)
         }
     }
 
     @Test
-    fun `test multi flow`() = runBlocking{
+    fun `test multiple values`() {
+        // simpleList().forEach { value -> println(value) }
 
+        // simpleSequence().forEach { value -> println(value) }
+    }
+
+    @Test
+    fun `test multiple values2`() = runBlocking<Unit> {
+        simpleList2().forEach { value -> println(value) }
+    }
+
+    @Test
+    fun `test multiple values3`() = runBlocking<Unit> {
         launch {
-            for(k in 1..3){
+            for (k in 1..3) {
                 println("I'm not blocked：$k")
                 delay(1500)
             }
         }
-        simpleFow().collect{
-            println(it)
+        simpleFow().collect { value -> println(value) }
+    }
+
+    fun simpleFow2() = flow<Int> {
+        println("Flow started")
+        for (i in 1..3) {
+            delay(1000)
+            emit(i)
         }
     }
 
     @Test
-    fun `test flow cold`() = runBlocking{
+    fun `test flow is cold`() = runBlocking {
         val flow = simpleFow2()
         println("Calling collect...")
-        flow.collect{
+        flow.collect {
             println(it)
         }
 
         println("Calling collect...again")
-        flow.collect{
-            println(it)
-        }
-
-        simpleFow2()
-            .flowOn(Dispatchers.IO)
-            .buffer(50)
-            .conflate()
-            .collect{
-                println(it)
-            }
-    }
-
-    private suspend fun performRequest(request:Int):String{
-        delay(1000)
-        return "respond $request"
-    }
-
-    // transform操作符
-    @Test
-    fun `test transform flow operator`() = runBlocking<Unit> {
-        (1..3).asFlow()
-            .transform {request->
-                emit("Making request $request")
-                emit(performRequest(request))
-            }.collect{
-                println(it)
-            }
-    }
-
-    // 限长操作符：take
-    @Test
-    fun `test take flow operator`() = runBlocking<Unit> {
-        (1..3).asFlow()
-            .take(2)
-            .collect{
-                println(it)
-            }
-    }
-
-    // 末端操作符：reduce
-    @Test
-    fun `test reduce flow operator`() = runBlocking<Unit> {
-        (1..5).asFlow()
-            .map{it*it}
-            .reduce{a,b -> a+b}
-            .run(::println)
-    }
-
-    // 组合操作符：zip
-    @Test
-    fun `test zip flow operator`() = runBlocking<Unit> {
-
-        val numbers =(1..5).asFlow()
-        val strs = flowOf("one","two","three")
-        numbers.zip(strs){a,b->
-            "$a -> $b"}
-            .collect{
+        flow.collect {
             println(it)
         }
     }
 
-    private fun requestFlow(i:Int) = flow<String> {
-        emit("$i:First")
-        delay(500)
-        emit("$i:Second")
+    @Test
+    fun `test flow continuation`() = runBlocking<Unit> {
+        (1..5).asFlow().filter {
+            it % 2 == 0
+        }.map {
+            "string $it"
+        }.collect {
+            println("collect $it")
+        }
     }
 
-    // 展平操作符：flatMapConcat
     @Test
-    fun `test flatMapConcat flow operator`() = runBlocking<Unit> {
-        val startTime = System.currentTimeMillis()
-        (1..3).asFlow()
-            .onEach { delay(100) }
-            .flatMapConcat { requestFlow(it) }  // 先拆分1..3  依次与requestFlow结合
-            .collect{
-                println("$it at ${System.currentTimeMillis() - startTime} ms from startTime")
-            }
+    fun `test flow builder`() = runBlocking<Unit> {
+//        flowOf("one", "two", "three")
+//            .onEach { delay(1000) }
+//            .collect {
+//                println("collect $it")
+//            }
+
+        (1..3).asFlow().collect(::println)
     }
 
-    // 展平操作符：flatMapMerge
-    @Test
-    fun `test flatMapMerge flow operator`() = runBlocking<Unit> {
-        val startTime = System.currentTimeMillis()
-        (1..3).asFlow()
-            .onEach { delay(100) }
-            .flatMapMerge { requestFlow(it) } // 先拆分requestFlow  依次与1..3结合
-            .collect{
-                println("$it at ${System.currentTimeMillis() - startTime} ms from startTime")
-            }
+    fun simpleFow3() = flow<Int> {
+        println("Flow started ${Thread.currentThread().name}")
+        for (i in 1..3) {
+            delay(1000)
+            emit(i)
+        }
     }
 
-    // 展平操作符：flatMapLatest
-    @Test
-    fun `test flatMapLatest flow operator`() = runBlocking<Unit> {
-        val startTime = System.currentTimeMillis()
-        (1..3).asFlow()
-            .onEach { delay(100) }
-            .flatMapLatest { requestFlow(it) } // 中间的不要了
-            .collect{
-                println("$it at ${System.currentTimeMillis() - startTime} ms from startTime")
+    fun simpleFow4() = flow {
+        withContext(Dispatchers.IO) {
+            println("Flow started ${Thread.currentThread().name}")
+            for (i in 1..3) {
+                delay(1000)
+                emit(i)
             }
+        }
+    }
+
+    @Test
+    fun `test flow context`() = runBlocking<Unit> {
+        simpleFow3().collect { value ->
+            println("Collected $value ${Thread.currentThread().name}")
+        }
+    }
+
+    fun simpleFow5() = flow<Int> {
+        println("Flow started ${Thread.currentThread().name}")
+        for (i in 1..3) {
+            delay(1000)
+            emit(i)
+        }
+    }.flowOn(Dispatchers.IO)
+
+    @Test
+    fun `test flow on`() = runBlocking<Unit> {
+        simpleFow5().collect { value ->
+            println("Collected $value ${Thread.currentThread().name}")
+        }
+    }
+
+    fun events() = (1..3)
+        .asFlow()
+        .onEach { delay(100) }
+        .flowOn(Dispatchers.Default)
+
+    @Test
+    fun `test flow launch`() = runBlocking<Unit> {
+        events().onEach { event -> println("event $event ${Thread.currentThread().name}") }
+            .launchIn(this)
+            .join()
+    }
+
+    fun simpleFow6() = flow<Int> {
+        println("Flow started ${Thread.currentThread().name}")
+        for (i in 1..3) {
+            delay(1000)
+            emit(i)
+            println("Emitting $i")
+        }
+    }
+
+    @Test
+    fun `test cancel flow`() = runBlocking<Unit> {
+        withTimeoutOrNull(2500) {
+            simpleFow6().collect(::println)
+        }
+        println("Done")
+    }
+
+    fun simpleFow7() = flow<Int> {
+        println("Flow started ${Thread.currentThread().name}")
+        for (i in 1..5) {
+            emit(i)
+            println("Emitting $i")
+        }
+    }
+
+    @Test
+    fun `test cancel flow check`() = runBlocking<Unit> {
+        /*simpleFow7().collect { value ->
+            println(value)
+            if (value == 3) {
+                cancel()
+            }
+        }*/
+        (1..5).asFlow().cancellable().collect { value ->
+            println(value)
+            if (value == 3) cancel()
+        }
+    }
+
+    fun simpleFow8() = flow<Int> {
+        for (i in 1..3) {
+            delay(100)
+            emit(i)
+            println("Emitting $i   ${Thread.currentThread().name}")
+        }
+    }
+
+    @Test
+    fun `test flow back pressure`() = runBlocking<Unit> {
+        val time = measureTimeMillis {
+            simpleFow8()
+                // .flowOn(Dispatchers.Default)
+                //.buffer(50)
+                .conflate()
+                .collect { value ->
+                    delay(300)
+                    println("Collected $value ${Thread.currentThread().name}")
+                }
+        }
+        println("Collected in $time ms")
     }
 }
--- kotlin/kotlincorountine/src/test/java/com/example/kotlincorountine/FlowTest02.kt
+++ kotlin/kotlincorountine/src/test/java/com/example/kotlincorountine/FlowTest02.kt
@@ -1,16 +1,133 @@
-package com.example.kotlincoroutine
+package com.example.kotlincorountine
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import java.lang.ArithmeticException
 
-// flow异常捕获
 class FlowTest02 {
 
+    private suspend fun performRequest(request: Int): String {
+        delay(1000)
+        return "respond $request"
+    }
+
+    // transform操作符
+    @Test
+    fun `test transform flow operator`() = runBlocking<Unit> {
+        /*(1..3).asFlow()
+            .map { request ->
+                performRequest(request)
+            }.collect {
+                println(it)
+            }*/
+
+        (1..3).asFlow()
+            .transform { request ->
+                emit("Making request $request")
+                emit(performRequest(request))
+            }.collect {
+                println(it)
+            }
+    }
+
+    fun numbers() = flow {
+        try {
+            emit(1)
+            emit(2)
+            println("This line will not execute")
+            emit(3)
+        } finally {
+            println("Finally in numbers")
+        }
+    }
+
+    // 限长操作符：take
+    @Test
+    fun `test limit length operator`() = runBlocking<Unit> {
+        numbers().take(2).collect { println(it) }
+    }
+
+    // 末端操作符：reduce
+    @Test
+    fun `test terminal operator`() = runBlocking<Unit> {
+        (1..5).asFlow()
+            .map { it * it }
+            .reduce { a, b -> a + b }
+            .run(::println)
+    }
+
+    // 组合操作符：zip
+    @Test
+    fun `test zip`() = runBlocking<Unit> {
+        val numbs = (1..4).asFlow()
+        val strs = flowOf("one", "two", "three")
+        numbs.zip(strs) { a, b ->
+            "$a -> $b"
+        }.collect {
+            println(it)
+        }
+    }
+
+    @Test
+    fun `test zip2`() = runBlocking<Unit> {
+        val numbs = (1..4).asFlow().onEach { delay(300) }
+        val strs = flowOf("one", "two", "three").onEach { delay(400) }
+        val startTime = System.currentTimeMillis()
+        numbs.zip(strs) { a, b ->
+            "$a -> $b"
+        }.collect {
+            println(it)
+            println("$it at ${System.currentTimeMillis() -startTime} ms from start")
+        }
+    }
+
+    private fun requestFlow(i: Int) = flow<String> {
+        emit("$i:First")
+        delay(500)
+        emit("$i:Second")
+    }
+
+    // 展平操作符：flatMapConcat
+    @Test
+    fun `test flatMapConcat flow operator`() = runBlocking<Unit> {
+        val startTime = System.currentTimeMillis()
+        (1..3).asFlow()
+            .onEach { delay(100) }
+            .flatMapConcat { requestFlow(it) }  // 先拆分1..3  依次与requestFlow结合
+            .collect {
+                println("$it at ${System.currentTimeMillis() - startTime} ms from startTime")
+            }
+    }
+
+    // 展平操作符：flatMapMerge
+    @Test
+    fun `test flatMapMerge flow operator`() = runBlocking<Unit> {
+        val startTime = System.currentTimeMillis()
+        (1..3).asFlow()
+            .onEach { delay(100) }
+            .flatMapMerge { requestFlow(it) } // 先拆分requestFlow  依次与1..3结合
+            .collect {
+                println("$it at ${System.currentTimeMillis() - startTime} ms from startTime")
+            }
+    }
+
+    // 展平操作符：flatMapLatest
+    @Test
+    fun `test flatMapLatest flow operator`() = runBlocking<Unit> {
+        val startTime = System.currentTimeMillis()
+        (1..3).asFlow()
+            .onEach { delay(100) }
+            .flatMapLatest { requestFlow(it) } // 中间的不要了
+            .collect {
+                println("$it at ${System.currentTimeMillis() - startTime} ms from startTime")
+            }
+    }
+
     private fun simpleFlow() = flow {
-        for(i in 1..3){
+        for (i in 1..3) {
             println("Emitting $i")
             emit(i)
         }
@@ -20,9 +137,9 @@ class FlowTest02 {
     @Test
     fun `test flow exception`() = runBlocking {
         try {
-            simpleFlow().collect{
+            simpleFlow().collect {
                 println(it)
-                check(it<=1){ "Collected $it"}
+                check(it <= 1) { "Collected $it" }
             }
         } catch (e: Exception) {
 
@@ -53,7 +170,7 @@ class FlowTest02 {
                     println(it)
                 } catch (e: Exception) {
                     //e.printStackTrace()
-                }finally {
+                } finally {
                     println("Done")
                 }
             }
@@ -73,7 +190,7 @@ class FlowTest02 {
                     println(it)
                 } catch (e: Exception) {
                     //e.printStackTrace()
-                }finally {
+                } finally {
                     println("Done")
                 }
             }
